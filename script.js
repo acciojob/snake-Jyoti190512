46e4691acb992d0885ffc00f25b84d25941bbd4d
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-18 09:55:10 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your code here
+// Constants
+const gameContainer = document.getElementById('gameContainer');
+const scoreBoard = document.querySelector('.scoreBoard');
+const pixelSize = 10;
+const rows = 40;
+const cols = 40;
+
+// Snake object
+let snake = {
+  body: [{ row: 20, col: 1 }],
+  direction: 'right',
+  // Add more properties as needed
+};
+
+// Food object
+let food = {
+  row: getRandomNumber(0, rows - 1),
+  col: getRandomNumber(0, cols - 1),
+};
+
+// Game loop
+setInterval(updateGame, 100);
+
+// Function to update the game state
+function updateGame() {
+  // Clear the game container
+  gameContainer.innerHTML = '';
+
+  // Update snake position
+
+  // Check for collision with food
+
+  // Check for collision with walls or self
+
+  // Render the game elements
+
+  // Update the score
+}
+
+// Function to generate a random number within a range
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
